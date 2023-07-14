c1f12707063a1a0bb6a4e94b8306378eafa6fd93
Artem Sibisov <artsib7777@yandex.ru>
2023-07-15 02:37:16 +0800

    преобразование в контроллере карточек для их создания createCArd

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -15,7 +15,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send({ data: card }))
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные при создании карточки.'));
