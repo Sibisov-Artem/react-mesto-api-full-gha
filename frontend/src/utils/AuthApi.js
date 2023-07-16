5c1062340991646be0a417a18149a12f896af587
Artem Sibisov <artsib7777@yandex.ru>
2023-07-16 20:51:54 +0800

    поправил обращение в Api, AuthApi на порт 3000

--- frontend/src/utils/AuthApi.js
+++ frontend/src/utils/AuthApi.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = ' https://auth.nomoreparties.co';
-export const BASE_URL = ' http://localhost:4000';
+export const BASE_URL = ' http://localhost:3000';
 
 function checkResponse(res) {
     if (res.ok) {
