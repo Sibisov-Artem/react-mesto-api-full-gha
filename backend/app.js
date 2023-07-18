3b11800bbff1f6c2309e9f125ed503573474afe4
Artem Sibisov <artsib7777@yandex.ru>
2023-07-18 09:52:59 +0800

    добавлен crash-test в App.js

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,12 @@ app.use(cors());
 
 app.use(requestLogger); // подключаем логгер запросов до всех обработчиков роутов
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.get('/', (req, res) => {
   res.send('проверка');
 });
