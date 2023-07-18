90ebbcb343865a337ad8b1f3e61d1af2cf3f8fbf
Artem Sibisov <artsib7777@yandex.ru>
2023-07-18 21:03:15 +0800

    поднял app.use(cors()) выше, сомневаюсь, что поможет в устранении ошибки

--- backend/app.js
+++ backend/app.js
@@ -18,15 +18,15 @@ const regularHttp = require('./utils/regularHttp');
 
 const { PORT = 3000 } = process.env;
 
+const app = express();
+
+app.use(cors());
+
 // mongoose.connect('mongodb://localhost:27017/mestodb');
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 }).then(() => {
   console.log('connected to db');
 });
-
-const app = express();
-
-app.use(cors());
 
 app.use(requestLogger); // подключаем логгер запросов до всех обработчиков роутов
 
